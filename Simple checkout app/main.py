86fb2eee79c065e08ef6444e106cb6add9a8856a
fen00b <fenobagus21@gmail.com>
2025-01-01 13:12:53 +0700

    tambah fungsi inventory (untuk melihat inventory

--- Simple checkout app/main.py
+++ Simple checkout app/main.py
@@ -12,10 +12,32 @@ def additem():
     print(f"{item_name} sudah dimasukan ke inventaris")
 
 def update_qty():
-    pass
+    item_name = input("Masukan nama barang :").strip()
+    if item_name not in inventory:
+        print("barang tidak ada di inventaris")
+        return
+    quantity = int(input("Masukan jumlah barang :"))
+    inventory[item_name]["quantity"]=quantity
+    print(f"Jumlah {item_name} diubah menjadi {quantity}")
 
-def show_item():
-    pass
+
+def inventory():
+    if not inventory:
+        print("Inventaris kosong!")
+        return
+    print("\n Daftar Inventaris :")
+    print(f"{'Barang':<20}{'Jumlah barang':<15}{'harga':<15}{'Nilai total':<15}")
+    print("="*65)
+
+    total_value = 0
+
+    for item, details in inventory.items():
+        item_value = details["quantity"] * details["price"]
+        total_value += item_value
+        print(f"{item:<20}{details['quantity']:<10}{details['price']:<10}{item_value:<10}")
+    print("-" * 50)
+    print(f"Total Inventory Value: {total_value:.2f}\n")
+
 
 def search_item():
     pass
@@ -23,3 +45,4 @@ def search_item():
 def remove_item():
     pass
 
+inventory()
